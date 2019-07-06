9874c2c8271ead8863a0a751b4824ba133201c2d
Kiharu Shishikura <mutobow0414@yahoo.co.jp>
2019-07-07 01:22:26 +0700

    edit python modules

--- pyxie/apputil/__init__.py
+++ pyxie/apputil/__init__.py
@@ -1,4 +1,38 @@
 """
-pyxie game engine
+pyxie game engine \n
+apprication utilities
 """
-from _pyxie import *
+import pyxie
+
+def createSprite(width:float=100, height:float=100, texture:str=None, uv_left_top:tuple=(0,0), uv_right_bottom:tuple=(1,1)):
+    """
+    Create Visible Rectangle \n
+    return : editableFigure
+    """
+    gen = pyxie.shaderGenerator()
+    if texture != None:
+        gen.setColorTexture(True)
+    gen.setBoneCondition(1, 1)
+
+    efig = pyxie.editableFigure("sprite")
+    efig.addMaterial("mate", gen)
+    efig.addMesh("mesh", "mate");
+
+    w = width/2
+    h = height/2
+    points = (-w,h, w,h, -w,-h, w,-h)
+    uvs = (0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0)
+    tris = (0, 2, 1, 1, 2, 3)
+
+    efig.setVertexElements("mesh", efig.VertexAttribure_POSITION, 0, 4, points, 2)
+    efig.setVertexElements("mesh", efig.VertexAttribure_UV0, 0, 4, uvs, 2)
+    efig.setTriangles("mesh", 0, 2, tris);
+    efig.addJoint("joint");
+    efig.setMaterialParam("mate", "DiffuseColor", (1.0, 1.0, 1.0, 1.0));
+
+    if texture != None:
+        efig.setMaterialParamTexture("mate", "ColorSampler", texture,
+                                     wrap_s=pyxie.SAMPLERSTATE_BORDER,wrap_t=pyxie.SAMPLERSTATE_BORDER,
+                                     minfilter=pyxie.SAMPLERSTATE_NEAREST, magfilter=pyxie.SAMPLERSTATE_NEAREST)
+        efig.setMaterialRenderState("mate", "blend_enable", True)
+    return efig
--- pyxie/devtool/__init__.py
+++ pyxie/devtool/__init__.py
@@ -1,4 +1,14 @@
 """
-pyxie game engine
+pyxie game engine offline develop tools
+
+convert 3d assets
+convert images to platform dipend format
+make pyxie package
+upload package to devserver
+
 """
-from _pyxie import *
+from pyxie.devtool._pyxietools import *
+TARGET_PLATFORM_PC = 0
+TARGET_PLATFORM_IOS = 1
+TARGET_PLATFORM_ANDROID = 2
+
