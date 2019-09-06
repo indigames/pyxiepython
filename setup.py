fe3240607e276929b942701d712d6ff14b31e55f
saruyamatarou <mutobow0414@yahoo.co.jp>
2019-09-06 10:43:49 +0700

    update

--- setup.py
+++ setup.py
@@ -1,6 +1,22 @@
+try:
+    importlib.import_module('numpy')
+except ImportError:
+	from pip._internal import main as _main
+	_main(['install', 'numpy'])
+
 from setuptools import setup, Extension, find_packages
 import setuptools
 import numpy
+import sys
+from distutils.sysconfig import get_python_lib
+
+
+is64Bit = sys.maxsize > 2 ** 32
+
+if is64Bit:
+    bindir = 'bin/win64'
+else:
+    bindir = 'bin/win32'
 
 pyxie_module = Extension('pyxie._pyxie', 
                        sources=[
@@ -19,16 +35,16 @@ pyxie_module = Extension('pyxie._pyxie',
                            'pythonTexture.cpp'
                        ],
                        include_dirs=['bin/include', numpy.get_include()],
-			           library_dirs=['bin/win32'],
+			           library_dirs=[bindir],
 			           libraries=['pyxcore', 'user32', 'Gdi32'])
 
 tools_module = Extension('pyxie.devtool._pyxietools', 
                        sources=['pythonTools.cpp'],
                        include_dirs=['bin/include', numpy.get_include()],
-			           library_dirs=['bin/win32'],
+			           library_dirs=[bindir],
 			           libraries=['pyxtools','pyxcore'])
 
-setup(name='pyxie', version='0.3.120',
+setup(name='pyxie', version='0.3.110',
 		description='pyxie game engine module',
 		author=u'Kiharu Shishikura',
 		author_email='shishi@indigames.net',
@@ -48,5 +64,12 @@ setup(name='pyxie', version='0.3.120',
 			'Operating System :: Microsoft :: Windows',
 			'Topic :: Games/Entertainment',
 		],
+        data_files=[
+            ('pyxie' ,  [bindir+"/pyxcore.dll"]),
+            ('pyxie/devtool',  [bindir+"/pyxtools.dll",bindir+"/PVRTexLib.dll"])
+        ],
         include_package_data=True
       )
+
+
+
