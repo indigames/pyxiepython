4536730c73ca4fcd16b676ab5f707e6fabfdb46c
saruyamatarou <mutobow0414@yahoo.co.jp>
2019-10-30 10:06:33 +0700

    update

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ tools_module = Extension('pyxie.devtool._pyxietools',
 			           library_dirs=[bindir],
 			           libraries=['pyxtools','pyxcore'])
 
-setup(name='pyxie', version='0.3.3',
+setup(name='pyxie', version='0.3.32',
 		description='pyxie game engine module',
 		author=u'Kiharu Shishikura',
 		author_email='shishi@indigames.net',
